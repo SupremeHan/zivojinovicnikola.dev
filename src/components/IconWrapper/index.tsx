508f4712e13c874d075f218af56463159b1827ea
Nikola Zivojinovic <zivojinovicn13@gmail.com>
2024-01-19 22:04:57 +0100

    Removed unused components

--- src/components/IconWrapper/index.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from "react";
-import "./IconWrapper.scss";
-
-export function IconWrapper({ children }: PropsWithChildren) {
-  return <div className="IconWrapper">{children}</div>;
-}
