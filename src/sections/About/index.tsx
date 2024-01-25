a0bc0fbd7fa30bb5330a3c6dd3735c82c176a75e
Nikola Zivojinovic <zivojinovicn13@gmail.com>
2024-01-25 21:29:10 +0100

    Updated about me text

--- src/sections/About/index.tsx
+++ src/sections/About/index.tsx
@@ -9,25 +9,27 @@ export function About() {
         </h2>
         <div className="About__wrapper">
           <p className="About__text">
-            Hey there! Let's get straight to it! I'm Casper, a motivated web developer and student
-            based in Belgium. I am an 18-year-old developer building cutting-edge web applications
-            using the latest technologies such as React.js, Next.js, TypeScript, and much more for
-            over 4 years now.
+            Hey there! Let's get straight to it! I'm Nikola, a motivated web developer currently
+            based in Serbia. I am 26 years old and have been passionately building cutting-edge web
+            applications using the latest technologies, including React.js, Next.js, TypeScript, and
+            more, for over 3 years now.
           </p>
 
           <p className="About__text">
-            I started back when I was a 14-year-old kid that had an extreme passion for creating
-            stuff, building simple static web pages and expanding my CSS knowledge by recreating
-            awesome designs. I've continued to grow and evolve as a developer, taking on new
-            challenges and learning the latest technologies along the way.
+            I became interested in web programming during college. At first, I started building
+            simple static web pages and expanding my CSS knowledge by recreating awesome designs.
+            Later, I discovered a framework called Angular, which piqued my interest even more in
+            the world of web applications. I've continued to grow and evolve as a developer, taking
+            on new challenges and learning the latest technologies along the way.
           </p>
 
           <p className="About__text">
-            When I'm not in full-on developer mode, you will find me blasting down a dirt track or
-            the streets of my city. There's nothing quite like the rush of adrenaline that comes
-            from going ultra-fast, and for me, there's no better way to experience that than on my
-            trusty mountain bike. I also have a deep appreciation for music, listening to a wide
-            range of genres that I thoroughly enjoy.
+            When I'm not in full-on developer mode, you will find me lifting weights at my local
+            CrossFit box or hiking in the forests of Serbia. If you were to ask me to choose between
+            going through an intense training session or walking in the calm, rainy forest, I would
+            have to give it a lot of thought because both activities bring me joy in their own way.
+            I also have a deep appreciation for music, listening to a wide range of genres that I
+            thoroughly enjoy.
           </p>
         </div>
       </div>
